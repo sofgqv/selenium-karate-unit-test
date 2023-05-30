6eb3737ae26ac3b401d95c2c1732cab7a479c2fa
Valeria Espinoza <76666534+ValDLaw@users.noreply.github.com>
2023-05-29 19:58:58 -0500

    fizzbuzz function

--- test/fizzbuzzmethod.py
+++ test/fizzbuzzmethod.py
@@ -1,11 +1,9 @@
-for fizzbuzz in range(51):
-    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
-        print("fizzbuzz")
-        continue
-    elif fizzbuzz % 3 == 0:
-        print("fizz")
-        continue
-    elif fizzbuzz % 5 == 0:
-        print("buzz")
-        continue
-    print(fizzbuzz)
+def fizzbuzz(i):
+    if i % 15 == 0:
+        return "FizzBuzz"
+    elif i % 3 == 0:
+        return "Fizz"
+    elif i % 5 == 0:
+        return "Buzz"
+    else:
+        return i
